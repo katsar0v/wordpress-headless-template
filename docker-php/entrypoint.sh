477632b0d73d10b59f1df423fe072c9c83b82e2d
Katsarov <me@katsarov.info>
2019-12-15 18:01:19 +0100

    fix rewrite structure - needed for rest api

--- docker-php/entrypoint.sh
+++ docker-php/entrypoint.sh
@@ -11,6 +11,7 @@ wp core download
 wp config create --skip-check --dbhost=database --dbname=wp --dbuser=root --dbpass=$MYSQL_ROOT_PASSWORD
 wp db create
 wp core install --admin_password=$WP_ADMIN_PASSWORD --url=localhost --title=Example --admin_user=$WP_ADMIN_USERNAME --admin_email=info@example.com --skip-email
+wp rewrite structure '/%postname%/'
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
