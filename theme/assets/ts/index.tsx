5ec957511f718fc6e3103913df3d2f6d4c561e9d
Katsarov <me@katsarov.info>
2019-12-15 20:26:31 +0100

    add simple loader with css

--- theme/assets/ts/index.tsx
+++ theme/assets/ts/index.tsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {WP_API} from './api';
-import {WP_Post, WP_Page} from './types'
 
-ReactDOM.render(
-    <h1>Hello, world!</h1>,
-    document.getElementById('root')
-);
 
-WP_API.get_posts().then((posts: WP_Post[]) => {
-    console.log("Posts: ", posts);
-})
-WP_API.get_pages().then((pages: WP_Page[]) => {
-    console.log("Pages: ", pages);
+Promise.all([
+    WP_API.get_posts(),
+    WP_API.get_pages()
+]).then((responses) => {
+    ReactDOM.render(
+        <h1>Hello, world!</h1>,
+        document.getElementById('root')
+    );
+    document.querySelector("body").classList.toggle("active");
 })
